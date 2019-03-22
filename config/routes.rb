e14a6e5ddacb7175c7ebe2e2aaec48af6e9f57dc
Walker Spence <walker.spence@gmail.com>
2019-03-21 20:16:44 -0700

    implemented events list

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :events
+  root 'home#home'
+
   mount JasmineRails::Engine => '/specs' if defined?(JasmineRails)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
