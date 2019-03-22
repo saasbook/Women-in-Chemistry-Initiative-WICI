7eec5ced5c9e9aa2bf1ad5a9033da6d48b76ad0a
Walker Spence <walker.spence@gmail.com>
2019-03-22 12:19:13 -0700

    added coverage for controller actions

---
+++ spec/factories/event.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :event do
+    name "Event"
+    description "asdlkfjas;ldkfjasdf"
+    date "25-Nov-2018"
+    location "someplace"
+    tickets "tickets.com"
+  end
+end
