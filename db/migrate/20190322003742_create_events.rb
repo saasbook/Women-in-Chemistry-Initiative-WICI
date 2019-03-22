14b269782832d7027b72634c2247f39503e67e45
Walker Spence <walker.spence@gmail.com>
2019-03-21 17:43:27 -0700

    first migration but i actually do git add first

---
+++ db/migrate/20190322003742_create_events.rb
@@ -0,0 +1,13 @@
+class CreateEvents < ActiveRecord::Migration[5.1]
+  def change
+    create_table :events do |t|
+      t.string :name
+      t.text :description
+      t.datetime :date
+      t.string :location
+      t.string :tickets
+
+      t.timestamps
+    end
+  end
+end
