b812f7c3d652a47e7bee48d7dc5833c8be959603
Nicole Perez-Bertolotto <nicolepb@Nicoles-MacBook-Air.local>
2019-03-26 00:21:15 +0100

    Contact Form with mailer

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,26 @@
+class MessagesController < ApplicationController
+
+  def new
+    @message = Message.new
+  end
+
+  def create
+    # if the Message is valid then we redirect to new_message_url,
+    # otherwise we stay inside the 'create' action and render the HTML form again.
+    @message = Message.new(message_params)
+    # call the Mailer if the message received from the user is valid
+    if @message.valid?
+      MessageMailer.contact_me(@message).deliver_now
+      redirect_to new_message_url, notice: "Message received, thanks!"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:name, :email, :body)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   resources :events
   mount JasmineRails::Engine => '/specs' if defined?(JasmineRails)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'contact-me', to: 'messages#new', as: 'new_message'
+  # ^ creates a route named new_message_url which maps GET requests
+  #for http://localhost:3000/contact-me to an action named MessagesController#new.
+  post 'contact-me', to: 'messages#create', as: 'create_message'
+  #This route specifies that POST requests to http://localhost:3000/contact-me
+  # should be handled by an action called MessagesController#create.
 end
