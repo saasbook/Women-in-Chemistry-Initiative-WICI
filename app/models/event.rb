3bf4ba649010ecd3ba469b826a2b1b2f5c6e7138
Walker Spence <walker.spence@gmail.com>
2019-04-04 15:15:09 -0700

    removed logic from EventsController

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,9 @@
 class Event < ApplicationRecord
+  # attr_accessor :name, :description, :date, :location, :tickets
+  def self.past_events
+    Event.where("date < ?", Date.current).order(date: :desc)
+  end
+  def self.future_events
+    Event.where("date > ?", Date.current).order(:date)
+  end
 end
