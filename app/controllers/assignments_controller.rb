3af9745139c62bc85a33db53d445ecb1b131a2fe
Danny Toth <dantoth24@berkeley.edu>
2019-04-12 19:34:15 -0700

    Allows volunteers with no task

---
+++ app/controllers/assignments_controller.rb
@@ -0,0 +1,65 @@
+class AssignmentsController < ApplicationController
+  before_action :set_event_and_task, only: [:index, :create, :new, :destroy]
+  before_action do
+    if !current_admin.nil?
+      authenticate_admin!
+      @user = current_admin
+    else
+      authenticate_volunteer!
+      @user = current_volunteer
+    end
+  end
+
+
+  def index
+    @assignments = @event.assignments
+    @info = @assignments.empty? ? "#{@event.name} has no volunteers." : ''
+  end
+  # GET /events/new
+  def new
+    @assignment = Assignment.new
+  end
+
+  # POST /events
+  # POST /events.json
+  def create
+
+
+
+    @assignment = assignment.new()
+
+    respond_to do |format|
+      if @assignment.save
+        format.html { redirect_to event_assignments_path(@event), notice: 'You are now a volunteer.' }
+        format.json { render :show, status: :created, location: @assignment.event }
+      else
+        format.html { render :new }
+        format.json { render json: @event.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /events/1
+  # DELETE /events/1.json
+  def destroy
+    @assignment.destroy
+    respond_to do |format|
+      format.html { redirect_to @event, notice: 'Volunteer was successfully unassigned.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+
+
+    def set_event_and_task
+      @event = Event.find(params[:event_id])
+      @task = Task.find_by_id(params[:task_id]) || Task.get_none_task(@event)
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def assignment_params
+      params.require(:assignment).permit(:task_id, :volunteer_id)
+    end
+end
--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,11 @@ class Task < ApplicationRecord
   has_many :assignments
   has_many :volunteers, :through => :assignments
   has_one :event
+
+  # This allows volunteers to sign up for events without a specific task.
+  # We may decide to remove this later, as we can stub tasks once they are done.
+  def self.get_none_task(event)
+    task = Task.find_by_title_and_event_id(:None, event) || Task.new(title: :None, event_id: event)
+    task
+  end
 end
