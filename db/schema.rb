2c57c0ef9f78d9d2f18eee4f5236fd2fe6daa23b
Aurash Jalalian <aurash@berkeley.edu>
2019-04-14 11:23:44 -0700

    removed accidently merged todolist features

--- db/schema.rb
+++ db/schema.rb
@@ -44,15 +44,7 @@ ActiveRecord::Schema.define(version: 20190411013744) do
     t.datetime "updated_at", null: false
     t.index ["event_id"], name: "index_guests_on_event_id"
   end
-
-  create_table "todolists", force: :cascade do |t|
-    t.string "event_name"
-    t.datetime "event_date"
-    t.text "description"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
+  
   create_table "volunteers", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
