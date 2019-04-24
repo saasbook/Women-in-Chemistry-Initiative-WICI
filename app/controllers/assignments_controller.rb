138c9aae6ca63961baa857cfa18ec3ec58848b3a
Nicole Perez-Bertolotto <nicole.pb@berkeley.edu>
2019-04-23 23:56:09 -0700

    remind tasks with deadline

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -30,8 +30,10 @@ class AssignmentsController < ApplicationController
 
     respond_to do |format|
       if @assignment.save
-        #HERE
         format.html { redirect_to event_task_path(@event, @task), notice: 'You are now a volunteer.' }
+        if @task.deadline.nil?
+          @task.deadline = @event.date
+        end
         RemindersMailer.remind_task(@assignment.volunteer, @task).deliver_later(wait_until: @task.deadline - 1)
         format.json { render :show, status: :created, location: @assignment.event }
       else
