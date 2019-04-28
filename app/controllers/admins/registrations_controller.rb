b3862bb0b709b43ae71b0412d80ef795211f9d5d
Danny Toth <dantoth24@berkeley.edu>
2019-04-27 17:33:03 -0700

    Integrating customer feedback and more class cleanup

--- app/controllers/admins/registrations_controller.rb
+++ app/controllers/admins/registrations_controller.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class Admins::RegistrationsController < Devise::RegistrationsController
   include Accessible
   skip_before_action :check_user, except: [:new, :create]
