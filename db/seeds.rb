a715a805819cecce0766e86ef6ca1ec4e2101674
Walker Spence <walker.spence@gmail.com>
2019-05-12 16:24:36 -0700

    fixed seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,10 @@
-Admin.create!([
-  {email: "tempadmin@wici.com", password: "WICIWebsite", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, firstname: nil, lastname: nil, approved: true}
-])
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
 
+Admin.create(email: "tempadmin@wici.com", approved: true, password: "WICIWebsite", password_confirmation: "WICIWebsite")
+Leader.create(firstname: "Kiera", lastname: "Wilhelm", role: "President")
