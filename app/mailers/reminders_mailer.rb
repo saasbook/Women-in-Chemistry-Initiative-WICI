94d3c9b86b2bc7046193ec4d9884646f182eb5cd
Walker Spence <walker.spence@gmail.com>
2019-05-11 19:03:29 -0700

    fixed merge conflict n/w

--- app/mailers/reminders_mailer.rb
+++ app/mailers/reminders_mailer.rb
@@ -1,13 +1,13 @@
 class RemindersMailer < ApplicationMailer
   def remind_guest(guest, event)
     @greeting = "Hello"
-    @body = "You have the WICI #{event.name}, on #{event.date}, at #{event.location}"
+    @body = "You have the WICI #{event.name}, on #{event.date.strftime('%A, %b%e, at %l:%M %p')}, at #{event.location}"
     mail to: guest.email, from: "donotreplywici@gmail.com", subject: "WICI Event Reminder"
   end
 
   def confirm_guest(guest, event, qrcode)
     @greeting = "Hello"
-    @body = "You are registered for the WICI #{event.name}, on #{event.date}, at #{event.location}"
+    @body = "You are registered for the WICI #{event.name}, on #{event.date.strftime('%A, %b%e, at %l:%M %p')}, at #{event.location}"
     File.open("/tmp/qrcode.png", "wb") do |f|
       f.write qrcode
     end
@@ -18,7 +18,7 @@ class RemindersMailer < ApplicationMailer
 
   def remind_task(volunteer, task)
     @greeting = "Hello"
-    @body = "You have the task #{task.name}, due by #{task.deadline}."
+    @body = "You have the task #{task.name}, due by #{task.deadline.strftime('%A, %b%e, at %l:%M %p')}."
     mail to: volunteer.email, from: "donotreplywici@gmail.com", subject: "WICI Task Volunteer Reminder"
   end
 end
